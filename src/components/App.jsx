83fd8c57974f11eef7c7c911e4a9a26ae75a09ed
Heyswor <andreydymov93@gmail.com>
2022-12-07 00:13:05 +0200

    base repo

---
+++ src/components/App.jsx
@@ -0,0 +1,66 @@
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+import React, { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  handleContactAdd = data => {
+    const { name } = data;
+
+    const findedContact = this.state.contacts.find(contact =>
+      contact.name.toLowerCase().includes(name.toLowerCase())
+    );
+
+    if (findedContact) {
+      alert(`${findedContact.name} is already in contacts`);
+      return;
+    } else {
+      this.setState(prevState => ({
+        contacts: [...prevState.contacts, data],
+      }));
+    }
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  render() {
+    const { filter, contacts } = this.state;
+    const lowercaseFilter = filter.toLocaleLowerCase();
+
+    const filtredContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(lowercaseFilter)
+    );
+
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm onSumbit={this.handleContactAdd} contacts={contacts} />
+
+        <h2>Contacts</h2>
+        <Filter filter={filter} onChange={this.changeFilter} />
+        <ContactList
+          contacts={filtredContacts}
+          deleteContact={this.deleteContact}
+        />
+      </div>
+    );
+  }
+}
